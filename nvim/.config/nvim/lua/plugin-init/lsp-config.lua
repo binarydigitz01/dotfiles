d48b542cf5a3c097ee4f007c6dd48c61a5fff2ac
ice-cube69 <arnavvijaywargiya@gmail.com>
2021-09-19 17:13:34 +0530

    added a fully working nvim-cmp configuration

--- nvim/.config/nvim/lua/plugin-init/lsp-config.lua
+++ nvim/.config/nvim/lua/plugin-init/lsp-config.lua
@@ -14,14 +14,12 @@
         require('luasnip').lsp_expand(args.body)
       end,
     },
-
     mapping = {
       ['<C-d>'] = cmp.mapping.scroll_docs(-4),
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
       ['<C-Space>'] = cmp.mapping.complete(),
       ['<C-e>'] = cmp.mapping.close(),
       ['<CR>'] = cmp.mapping.confirm({ select = true }),
-      ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item())
     },
     sources = {
       { name = 'nvim_lsp' },
